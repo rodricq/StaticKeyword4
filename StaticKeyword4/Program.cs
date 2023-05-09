8f9a0e3e2af9af07c9300e283c1c968ff75806d1
beverley-dick <beverleydick11@gmail.com>
2023-05-09 17:55:16 +0300

    Add project files.

---
+++ StaticKeyword4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticKeyword4
+{
+   
+       static class Test
+        {
+            static int a = 5;
+            static void display()
+            {
+                Console.WriteLine("Static method");
+            }
+
+            static void Main(string[] args)
+            {
+                //creating object of Test
+               // Test t1 = new Test();
+                Console.WriteLine(a);
+                display();
+            }
+        }
+    }
+
